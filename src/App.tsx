f5abf2e6e01663dac4bcedf240eda441fbe72770
Dams4K <dams4k@users.noreply.github.com>
2025-06-20 19:17:44 +0200

    test cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCookies } from "react-cookie"
 
-function App() {
-  const [count, setCount] = useState(0)
+function MyForm() {
+  const [cookies, setCookie, removeCookie] = useCookies(["player-name"]);
+
+  function onChange(newPlayerName: string): void {
+    setCookie("player-name", newPlayerName);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form>
+      <label>Player name:
+        <input type="text" defaultValue={cookies["player-name"]} onChange={evt => onChange(evt.target.value)}/>
+      </label>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      Hello world
+      <MyForm />
+    </div>
+    // <div>
+    //   <input name={cookies["player-name"] || ''} defaultValue={"Dams4K"} onChange={evt => onChange(evt.target.value)} />
+    //   {cookies["player-name"] && <h1>Hello {cookies["player-name"]}!</h1>}
+    // </div>
   )
 }
 
